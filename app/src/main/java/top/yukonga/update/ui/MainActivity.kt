4fd8a708701b7c2e040675cbb292ca6a9314cc06
Akane Beneckendorff <akane@akanework.org>
2023-12-07 14:39:29 +0800

    Updater: Refactor some strings
    Signed-off-by: Akane Beneckendorff <akane@akanework.org>

--- app/src/main/java/top/yukonga/update/ui/MainActivity.kt
+++ app/src/main/java/top/yukonga/update/ui/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
 import android.os.Bundle
-import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
 import android.widget.Toast
@@ -74,75 +73,92 @@ class MainActivity : AppCompatActivity() {
 
                     val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
                     withContext(Dispatchers.Main) {
-                        val romDevice = romInfo.currentRom?.device
-                        val romVersion = romInfo.currentRom?.version
-                        val romBigVersion = romInfo.currentRom?.bigversion?.replace("816", "HyperOS 1.0")
-                        val codebase = romInfo.currentRom?.codebase
-                        val romBranch = romInfo.currentRom?.branch
-                        val romFileName = romInfo.currentRom?.filename
-                        val romFileSize = romInfo.currentRom?.filesize
-                        val romChangelog = romInfo.currentRom?.changelog
-                        val romMd5 = romInfo.currentRom?.md5
-                        val latestRomFileName = romInfo.latestRom?.filename
-                        val latestRomMd5 = romInfo.latestRom?.md5
 
                         // Show a toast if we didn't get anything from request
-                        if (romBranch == null) {
+                        if (romInfo.currentRom?.branch == null) {
                             Toast.makeText(
                                 this@MainActivity,
-                                "未获取到任何信息",
+                                getString(R.string.toast_no_info),
                                 Toast.LENGTH_SHORT
                             ).show()
                         }
 
-                        codenameInfo.setTextAnimation(romDevice)
+                        // Setup TextViews
+                        codenameInfo.setTextAnimation(romInfo.currentRom?.device)
 
-                        systemInfo.setTextAnimation(romVersion)
+                        systemInfo.setTextAnimation(romInfo.currentRom?.version)
 
-                        bigVersionInfo.setTextAnimation(romBigVersion)
-
-                        codebaseInfo.setTextAnimation(codebase)
-
-                        branchInfo.setTextAnimation(romBranch)
-
-                        filenameInfo.setTextAnimation(romFileName)
-
-                        filesizeInfo.setTextAnimation(romFileSize)
-
-                        downloadInfo.setTextAnimation(
-                            if (romMd5 == latestRomMd5)
-                                "https://ultimateota.d.miui.com/${romVersion}/${latestRomFileName}"
-                            else
-                                "https://bigota.d.miui.com/${romVersion}/${romFileName}"
+                        bigVersionInfo.setTextAnimation(
+                            romInfo.currentRom?.bigversion?.replace("816", "HyperOS 1.0")
                         )
 
-                        downloadInfo.setOnClickListener {
-                            val clip = ClipData.newPlainText("label", downloadInfo.text)
-                            clipboard.setPrimaryClip(clip)
-                            Toast.makeText(this@MainActivity, "链接已复制到剪贴板", Toast.LENGTH_SHORT).show()
-                        }
+                        codebaseInfo.setTextAnimation(romInfo.currentRom?.codebase)
+
+                        branchInfo.setTextAnimation(romInfo.currentRom?.branch)
+
+                        filenameInfo.setTextAnimation(romInfo.currentRom?.filename)
+
+                        filesizeInfo.setTextAnimation(romInfo.currentRom?.filesize)
+
+                        downloadInfo.setTextAnimation(
+                            if (romInfo.currentRom?.md5 == romInfo.latestRom?.md5)
+                                getString(
+                                    R.string.https_ultimateota_d_miui_com,
+                                    romInfo.currentRom?.version,
+                                    romInfo.latestRom?.filename
+                                )
+                            else
+                                getString(
+                                    R.string.https_bigota_d_miui_com,
+                                    romInfo.currentRom?.version,
+                                    romInfo.currentRom?.filename
+                                )
+                        )
 
                         val log = StringBuilder()
-                        romChangelog!!.forEach {
-                            log.append(it.key).append("\n").append(it.value.txt.joinToString("\n")).append("\n")
+                        romInfo.currentRom?.changelog!!.forEach {
+                            log.append(it.key)
+                                .append("\n")
+                                .append(
+                                    it.value.txt.joinToString("\n")
+                                )
+                                .append("\n")
                         }
 
                         changelogInfo.setTextAnimation(
                             log.toString()
                         )
 
-                        changelogInfo.apply {
-                            setOnClickListener {
+                        changelogInfo.setOnClickListener {
                                 val clip = ClipData.newPlainText("label", changelogInfo.text)
                                 clipboard.setPrimaryClip(clip)
-                                Toast.makeText(this@MainActivity, "更新日志已复制到剪贴板", Toast.LENGTH_SHORT).show()
-                            }
+                                Toast.makeText(
+                                    this@MainActivity,
+                                    getString(R.string.toast_copied_to_pasteboard),
+                                    Toast.LENGTH_SHORT
+                                ).show()
                         }
-                    }
-                }
-            }
-        }
-    }
+
+                        downloadInfo.setOnClickListener {
+                            val clip = ClipData.newPlainText(
+                                "label", downloadInfo.text
+                            )
+                            clipboard.setPrimaryClip(clip)
+                            Toast.makeText(
+                                this@MainActivity,
+                                getString(R.string.toast_copied_to_pasteboard),
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    } // Main context
+
+                } // CoroutineScope
+
+            } // Fab operation
+
+        } // Main content
+
+    } // OnResume
 
     override fun onDestroy() {
         super.onDestroy()
