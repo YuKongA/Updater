5a06eb5ea53b2505ae48fded59168c5655d91897
xiao_wine <xiaowine0@gmail.com>
2023-12-07 13:30:57 +0800

    优化布局

--- app/src/main/java/top/yukonga/update/activity/MainActivity.kt
+++ app/src/main/java/top/yukonga/update/activity/MainActivity.kt
@@ -11,141 +11,130 @@ import android.widget.AutoCompleteTextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.widget.doAfterTextChanged
-import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton
-import com.google.android.material.textfield.TextInputLayout
-import com.google.android.material.textview.MaterialTextView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import top.yukonga.update.R
 import top.yukonga.update.data.RomInfo
+import top.yukonga.update.databinding.ActivityMainBinding
+import top.yukonga.update.databinding.MainContentBinding
 import top.yukonga.update.utils.JsonUtils.parseJSON
 import top.yukonga.update.utils.Utils
 
 class MainActivity : AppCompatActivity() {
 
+
+    private var _activityMainBinding: ActivityMainBinding? = null
+    private val activityMainBinding get() = _activityMainBinding!!
+    private val mainContentBinding: MainContentBinding get() = _activityMainBinding!!.mainContent
+
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-
+        _activityMainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(activityMainBinding.root)
         val dropDownList = arrayOf("12", "13", "14")
         val adapter: ArrayAdapter<String> = ArrayAdapter<String>(this, R.layout.dropdown_list_item, dropDownList)
-
-        val codename = findViewById<TextInputLayout>(R.id.code_name)
-        codename.editText?.setText("houji")
-
-        val system = findViewById<TextInputLayout>(R.id.system_version)
-        system.editText?.setText("OS1.0.25.0.UNCCNXM")
-
-        val android = findViewById<TextInputLayout>(R.id.android_version)
-        android.editText?.setText("14")
-        (android.editText as? AutoCompleteTextView)?.setAdapter(adapter)
+        mainContentBinding.apply {
+            codeName.editText?.setText("houji")
+            systemVersion.editText?.setText("OS1.0.25.0.UNCCNXM")
+            androidVersion.editText?.setText("14")
+            (androidVersion.editText as? AutoCompleteTextView)?.setAdapter(adapter)
+        }
     }
 
     override fun onResume() {
         super.onResume()
-        val codename = findViewById<TextInputLayout>(R.id.code_name)
-        var codenameText = codename.editText?.text.toString()
-        codename.editText?.doAfterTextChanged {
-            codenameText = it.toString()
-        }
-
-        val system = findViewById<TextInputLayout>(R.id.system_version)
-        var systemText = system.editText?.text.toString()
-        system.editText?.doAfterTextChanged {
-            systemText = it.toString()
-        }
-
-        val android = findViewById<TextInputLayout>(R.id.android_version)
-        var androidText = android.editText?.text.toString()
-        android.editText?.doAfterTextChanged {
-            androidText = it.toString()
-        }
-
-        val implement = findViewById<ExtendedFloatingActionButton>(R.id.implement)
-        implement.setOnClickListener {
-            CoroutineScope(Dispatchers.Default).launch {
-                val romInfo = Utils.getRomInfo(codenameText, systemText, androidText).parseJSON<RomInfo>()
-                val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
-                withContext(Dispatchers.Main) {
-                    val romDevice = romInfo.currentRom?.device
-                    val romVersion = romInfo.currentRom?.version
-                    val romBigVersion = romInfo.currentRom?.bigversion?.replace("816", "HyperOS 1.0")
-                    val codebase = romInfo.currentRom?.codebase
-                    val romBranch = romInfo.currentRom?.branch
-                    val romFileName = romInfo.currentRom?.filename
-                    val romFileSize = romInfo.currentRom?.filesize
-                    val romChangelog = romInfo.currentRom?.changelog
-                    val romMd5 = romInfo.currentRom?.md5
-
-                    val latestRomFileName = romInfo.latestRom?.filename
-                    val latestRomMd5 = romInfo.latestRom?.md5
-
-                    if (romBranch == null) Toast.makeText(this@MainActivity, "未获取到任何信息", Toast.LENGTH_SHORT).show()
-
-                    val codeNameV = findViewById<MaterialTextView>(R.id.codename)
-                    codeNameV.visibility = if (romDevice != null) View.VISIBLE else View.GONE
-                    val codeNameInfo = findViewById<MaterialTextView>(R.id.codename_info)
-                    codeNameInfo.visibility = if (romDevice != null) View.VISIBLE else View.GONE
-                    codeNameInfo.text = romDevice
-                    val systemV = findViewById<MaterialTextView>(R.id.system)
-                    systemV.visibility = if (romVersion != null) View.VISIBLE else View.GONE
-                    val systemInfo = findViewById<MaterialTextView>(R.id.system_info)
-                    systemInfo.visibility = if (romVersion != null) View.VISIBLE else View.GONE
-                    systemInfo.text = romVersion
-                    val bigVersionV = findViewById<MaterialTextView>(R.id.big_version)
-                    bigVersionV.visibility = if (romBigVersion != null) View.VISIBLE else View.GONE
-                    val bigVersionInfo = findViewById<MaterialTextView>(R.id.big_version_info)
-                    bigVersionInfo.visibility = if (romBigVersion != null) View.VISIBLE else View.GONE
-                    bigVersionInfo.text = romBigVersion
-                    val codeVaseV = findViewById<MaterialTextView>(R.id.codebase)
-                    codeVaseV.visibility = if (codebase != null) View.VISIBLE else View.GONE
-                    val codeBaseInfo = findViewById<MaterialTextView>(R.id.codebase_info)
-                    codeBaseInfo.visibility = if (codebase != null) View.VISIBLE else View.GONE
-                    codeBaseInfo.text = codebase
-                    val branchV = findViewById<MaterialTextView>(R.id.branch)
-                    branchV.visibility = if (romBranch != null) View.VISIBLE else View.GONE
-                    val branchInfo = findViewById<MaterialTextView>(R.id.branch_info)
-                    branchInfo.visibility = if (romBranch != null) View.VISIBLE else View.GONE
-                    branchInfo.text = romBranch
-                    val fileNameV = findViewById<MaterialTextView>(R.id.filename)
-                    fileNameV.visibility = if (romFileName != null) View.VISIBLE else View.GONE
-                    val fileNameInfo = findViewById<MaterialTextView>(R.id.filename_info)
-                    fileNameInfo.visibility = if (romFileName != null) View.VISIBLE else View.GONE
-                    fileNameInfo.text = romFileName
-                    val fileSizeV = findViewById<MaterialTextView>(R.id.filesize)
-                    fileSizeV.visibility = if (romFileSize != null) View.VISIBLE else View.GONE
-                    val fileSizeInfo = findViewById<MaterialTextView>(R.id.filesize_info)
-                    fileSizeInfo.visibility = if (romFileSize != null) View.VISIBLE else View.GONE
-                    fileSizeInfo.text = romFileSize
-                    val downloadV = findViewById<MaterialTextView>(R.id.download)
-                    downloadV.visibility = if (romMd5 != null) View.VISIBLE else View.GONE
-                    val downloadInfo = findViewById<MaterialTextView>(R.id.download_info)
-                    downloadInfo.visibility = if (romMd5 != null) View.VISIBLE else View.GONE
-                    downloadInfo.text =
-                        if (romMd5 == latestRomMd5) "https://ultimateota.d.miui.com/${romVersion}/${latestRomFileName}" else "https://bigota.d.miui.com/${romVersion}/${romFileName}"
-                    downloadInfo.setOnClickListener {
-                        val clip = ClipData.newPlainText("label", downloadInfo.text)
-                        clipboard.setPrimaryClip(clip)
-                        Toast.makeText(this@MainActivity, "链接已复制到剪贴板", Toast.LENGTH_SHORT).show()
-                    }
-                    val changelogV = findViewById<MaterialTextView>(R.id.changelog)
-                    changelogV.visibility = if (romMd5 != null) View.VISIBLE else View.GONE
-                    val changelogInfo = findViewById<MaterialTextView>(R.id.changelog_info)
-                    changelogInfo.visibility = if (romMd5 != null) View.VISIBLE else View.GONE
-                    changelogInfo.text =
-                        romChangelog.toString().replace("{", "").replace("=txt=[", "\n").replace("]", "").replace(",", "\n").replace("}", "").replace(" ", "")
-                    changelogInfo.setOnClickListener {
-                        val clip = ClipData.newPlainText("label", changelogInfo.text)
-                        clipboard.setPrimaryClip(clip)
-                        Toast.makeText(this@MainActivity, "更新日志已复制到剪贴板", Toast.LENGTH_SHORT).show()
+        mainContentBinding.apply {
+            activityMainBinding.implement.setOnClickListener {
+                CoroutineScope(Dispatchers.Default).launch {
+                    val codenameText = codeName.editText?.text.toString()
+                    val systemText = systemVersion.editText?.text.toString()
+                    val androidText = androidVersion.editText?.text.toString()
+                    val romInfo = Utils.getRomInfo(codenameText, systemText, androidText).parseJSON<RomInfo>()
+                    val clipboard = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+                    withContext(Dispatchers.Main) {
+                        val romDevice = romInfo.currentRom?.device
+                        val romVersion = romInfo.currentRom?.version
+                        val romBigVersion = romInfo.currentRom?.bigversion?.replace("816", "HyperOS 1.0")
+                        val codebase = romInfo.currentRom?.codebase
+                        val romBranch = romInfo.currentRom?.branch
+                        val romFileName = romInfo.currentRom?.filename
+                        val romFileSize = romInfo.currentRom?.filesize
+                        val romChangelog = romInfo.currentRom?.changelog
+                        val romMd5 = romInfo.currentRom?.md5
+                        val latestRomFileName = romInfo.latestRom?.filename
+                        val latestRomMd5 = romInfo.latestRom?.md5
+                        if (romBranch == null) Toast.makeText(this@MainActivity, "未获取到任何信息", Toast.LENGTH_SHORT).show()
+                        codename.visibility = if (romDevice != null) View.VISIBLE else View.GONE
+                        codenameInfo.apply {
+                            visibility = if (romDevice != null) View.VISIBLE else View.GONE
+                            text = romDevice
+                        }
+                        system.visibility = if (romVersion != null) View.VISIBLE else View.GONE
+                        systemInfo.apply {
+                            visibility = if (romVersion != null) View.VISIBLE else View.GONE
+                            text = romVersion
+                        }
+                        bigVersion.visibility = if (romBigVersion != null) View.VISIBLE else View.GONE
+                        bigVersionInfo.apply {
+                            visibility = if (romBigVersion != null) View.VISIBLE else View.GONE
+                            text = romBigVersion
+                        }
+                        mainContentBinding.codebase.visibility = if (codebase != null) View.VISIBLE else View.GONE
+                        codebaseInfo.apply {
+                            visibility = if (codebase != null) View.VISIBLE else View.GONE
+                            text = codebase
+                        }
+                        branch.visibility = if (romBranch != null) View.VISIBLE else View.GONE
+                        branchInfo.apply {
+                            visibility = if (romBranch != null) View.VISIBLE else View.GONE
+                            text = romBranch
+                        }
+                        filename.visibility = if (romFileName != null) View.VISIBLE else View.GONE
+                        filenameInfo.apply {
+                            visibility = if (romFileName != null) View.VISIBLE else View.GONE
+                            text = romFileName
+                        }
+                        filesize.visibility = if (romFileSize != null) View.VISIBLE else View.GONE
+                        filesizeInfo.apply {
+                            visibility = if (romFileSize != null) View.VISIBLE else View.GONE
+                            text = romFileSize
+                        }
+                        download.visibility = if (romMd5 != null) View.VISIBLE else View.GONE
+                        downloadInfo.apply {
+                            visibility = if (romMd5 != null) View.VISIBLE else View.GONE
+                            text = if (romMd5 == latestRomMd5) "https://ultimateota.d.miui.com/${romVersion}/${latestRomFileName}" else "https://bigota.d.miui.com/${romVersion}/${romFileName}"
+                        }
+                        downloadInfo.setOnClickListener {
+                            val clip = ClipData.newPlainText("label", downloadInfo.text)
+                            clipboard.setPrimaryClip(clip)
+                            Toast.makeText(this@MainActivity, "链接已复制到剪贴板", Toast.LENGTH_SHORT).show()
+                        }
+                        changelog.visibility = if (romMd5 != null) View.VISIBLE else View.GONE
+                        changelogInfo.apply {
+                            visibility = if (romMd5 != null) View.VISIBLE else View.GONE
+                            val log = StringBuilder()
+                            romChangelog!!.forEach {
+                                log.append(it.key).append("\n").append(it.value.txt.joinToString("\n")).append("\n")
+                            }
+                            text = log.toString()
+                            setOnClickListener {
+                                val clip = ClipData.newPlainText("label", changelogInfo.text)
+                                clipboard.setPrimaryClip(clip)
+                                Toast.makeText(this@MainActivity, "更新日志已复制到剪贴板", Toast.LENGTH_SHORT).show()
+                            }
+                        }
                     }
                 }
             }
         }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _activityMainBinding = null
     }
 }
