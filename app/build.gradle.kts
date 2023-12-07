07b2539e427ed36652c5e30954c6e7d6ec3fe6da
Akane Beneckendorff <akane@akanework.org>
2023-12-07 14:40:41 +0800

    Updater: Lower minSdk version to 26
    Signed-off-by: Akane Beneckendorff <akane@akanework.org>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "top.yukonga.update"
-        minSdk = 31
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
