e1c9caf11960c27308d42a648df67c4037cff94d
淡い夏 <93428659+lightsummer233@users.noreply.github.com>
2023-12-18 20:28:05 +0800

    Updater: Shrink Extended FAB when RomInfo is displayed & Adjust UI (#4)
    * Updater: Shrink Extended FAB when RomInfo is displayed
    
    * Updater: Adjust UI
    
    * Updater: Adjust UI
    
    * Updater: Adjust UI
    像素级调整 TopAppBar 两边图标边距。

--- app/src/main/java/top/yukonga/update/ui/MainActivity.kt
+++ app/src/main/java/top/yukonga/update/ui/MainActivity.kt
@@ -172,8 +172,11 @@ class MainActivity : AppCompatActivity() {
 
                             // Show a toast if we didn't get anything from request
                             if (romInfo.currentRom?.branch == null) {
+                                activityMainBinding.implement.extend()
                                 MiuiStringToast.showStringToast(this@MainActivity, getString(R.string.toast_no_info), 0)
                                 throw NoSuchFieldException()
+                            } else {
+                                activityMainBinding.implement.shrink()
                             }
 
                             firstViewTitleArray.forEach {
