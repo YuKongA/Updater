a3b00b797565bfa2926e5cd77d605938e542142b
hosizoraru <hosizoraru@gmail.com>
2024-01-11 16:16:30 +0800

    Updater: Optimize download experience (#11)
    Co-authored-by: YuKongA <70465933+YuKongA@users.noreply.github.com>

--- app/src/main/kotlin/top/yukonga/update/logic/utils/FileUtils.kt
+++ app/src/main/kotlin/top/yukonga/update/logic/utils/FileUtils.kt
@@ -4,6 +4,8 @@ import android.app.DownloadManager
 import android.content.Context
 import android.os.Environment
 import androidx.core.net.toUri
+import top.yukonga.update.R
+import top.yukonga.update.logic.utils.miuiStringToast.MiuiStringToast.showStringToast
 import java.io.File
 
 object FileUtils {
@@ -37,6 +39,7 @@ object FileUtils {
             setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS, fileName)
             val downloadManager = context.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
             downloadManager.enqueue(this)
+            showStringToast(context, context.getString(R.string.download_start), 1)
         }
     }
 }
