87e3610ff2e4636b14412caab374a727ea79cd88
Yiğit <40565628+tryigit@users.noreply.github.com>
2024-01-16 15:52:22 +0300

    Update NetworkUtils.kt

--- app/src/main/kotlin/top/yukonga/update/logic/utils/NetworkUtils.kt
+++ app/src/main/kotlin/top/yukonga/update/logic/utils/NetworkUtils.kt
@@ -4,17 +4,20 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody
 import okhttp3.Response
+import java.io.IOException
 
 object NetworkUtils {
 
+    private val client = OkHttpClient()
+
+    @Throws(IOException::class)
     fun getRequest(url: String): Response {
-        val client = OkHttpClient()
         val request = Request.Builder().url(url).build()
         return client.newCall(request).execute()
     }
 
+    @Throws(IOException::class)
     fun postRequest(url: String, body: RequestBody): Response {
-        val client = OkHttpClient()
         val request = Request.Builder().url(url).post(body).build()
         return client.newCall(request).execute()
     }
