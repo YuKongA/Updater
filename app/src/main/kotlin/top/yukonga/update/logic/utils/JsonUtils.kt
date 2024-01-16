edd9f4ac54bf6ee0ab66ecafca04ffe63c8ff8a7
Yiğit <40565628+tryigit@users.noreply.github.com>
2024-01-16 15:48:17 +0300

    Update JsonUtils.kt

--- app/src/main/kotlin/top/yukonga/update/logic/utils/JsonUtils.kt
+++ app/src/main/kotlin/top/yukonga/update/logic/utils/JsonUtils.kt
@@ -3,5 +3,7 @@ package top.yukonga.update.logic.utils
 import kotlinx.serialization.json.Json
 
 object JsonUtils {
-    val json = Json { ignoreUnknownKeys = true }
+    fun getJson(ignoreUnknownKeys: Boolean = true): Json {
+        return Json { this.ignoreUnknownKeys = ignoreUnknownKeys }
+    }
 }
