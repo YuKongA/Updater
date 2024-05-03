5cfbc739931b522d57c294a4fea0e6f98ebfb145
余空 <70465933+YuKongA@users.noreply.github.com>
2024-05-03 12:24:06 +0800

    feat: Cleanup MiuiStringToast

--- app/src/main/kotlin/top/yukonga/miuiStringToast/MiuiStringToast.kt
+++ app/src/main/kotlin/top/yukonga/miuiStringToast/MiuiStringToast.kt
@@ -1,5 +1,6 @@
 package top.yukonga.miuiStringToast
 
+import StringToastBundle
 import android.annotation.SuppressLint
 import android.app.PendingIntent
 import android.content.Context
@@ -23,45 +24,25 @@ import top.yukonga.update.logic.utils.XiaomiUtils.isMiPad
 
 object MiuiStringToast {
 
-    fun newIconParams(
-        category: String?,
-        iconResName: String?,
-        iconType: Int,
-        iconFormat: String?
-    ): IconParams {
-        return IconParams(category, iconFormat, iconResName, iconType)
-    }
-
     @SuppressLint("WrongConstant")
-    fun showStringToast(
-        context: Context,
-        text: String?,
-        colorType: Int?
-    ) {
+    fun showStringToast(context: Context, text: String?, colorType: Int) {
         if ((!isMiPad() && isLandscape()) || !atLeast(Build.VERSION_CODES.TIRAMISU) || !isHyperOS()) {
             Toast.makeText(context, text, Toast.LENGTH_SHORT).show()
             return
         }
         try {
-            val textParams = TextParams(text, if (colorType == 1) colorToInt("#4CAF50") else colorToInt("#E53935"))
+            val textParams = TextParams(text, if (colorType == 1) Color.parseColor("#4CAF50") else Color.parseColor("#E53935"))
             val left = Left(textParams = textParams)
-            val iconParams: IconParams = newIconParams(Category.DRAWABLE, if (colorType == 1) "ic_update_toast" else "ic_update_toast_error", 1, FileType.SVG)
+            val iconParams = IconParams(Category.DRAWABLE, FileType.SVG, "ic_launcher", 1)
             val right = Right(iconParams = iconParams)
             val stringToastBean = StringToastBean(left, right)
             val jsonStr = Json.encodeToString(StringToastBean.serializer(), stringToastBean)
             val bundle = StringToastBundle.Builder()
                 .setPackageName(BuildConfig.APPLICATION_ID)
-                .setStrongToastCategory(StrongToastCategory.TEXT_BITMAP_INTENT.value)
+                .setStrongToastCategory(StrongToastCategory.TEXT_BITMAP_INTENT)
                 .setTarget(PendingIntent.getActivity(context, 0, Intent(context, MainActivity::class.java), PendingIntent.FLAG_IMMUTABLE))
-                .setDuration(2500L)
-                .setLevel(0.0f)
-                .setRapidRate(0.0f)
-                .setCharge(null as String?)
-                .setStringToastChargeFlag(0)
                 .setParam(jsonStr)
-                .setStatusBarStrongToast("show_custom_strong_toast")
                 .onCreate()
-
             val service = context.getSystemService(Context.STATUS_BAR_SERVICE)
             service.javaClass.getMethod(
                 "setStatus", Int::class.javaPrimitiveType, String::class.java, Bundle::class.java
@@ -69,13 +50,6 @@ object MiuiStringToast {
         } catch (e: Exception) {
             throw RuntimeException(e)
         }
-    }
-
-    fun colorToInt(color: String?): Int {
-        val color1 = Color.parseColor(color)
-        val color2 = Color.parseColor("#FFFFFF")
-        val color3 = color1 xor color2
-        return color3.inv()
     }
 
     object Category {
@@ -91,12 +65,12 @@ object MiuiStringToast {
         const val SVG = "svg"
     }
 
-    enum class StrongToastCategory(var value: String) {
-        VIDEO_TEXT("video_text"),
-        VIDEO_BITMAP_INTENT("video_bitmap_intent"),
-        TEXT_BITMAP("text_bitmap"),
-        TEXT_BITMAP_INTENT("text_bitmap_intent"),
-        VIDEO_TEXT_TEXT_VIDEO("video_text_text_video")
+    object StrongToastCategory {
+        const val VIDEO_TEXT = "video_text"
+        const val VIDEO_BITMAP_INTENT = "video_bitmap_intent"
+        const val TEXT_BITMAP = "text_bitmap"
+        const val TEXT_BITMAP_INTENT = "text_bitmap_intent"
+        const val VIDEO_TEXT_TEXT_VIDEO = "video_text_text_video"
     }
 
 }
